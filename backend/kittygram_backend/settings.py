ce90f04efc12f3771edf118b16ceb4af0abd09bc
katya-voronova <katya@example.com>
2025-09-28 21:11:01 +0300

    update

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,10 +95,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = '/app/static'
+MEDIA_ROOT = '/app/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
